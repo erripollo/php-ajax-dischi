426aaff361638d1ab8239f3bae476f5083494b0c
erripollo <pollone.enrico@gmail.com>
2021-06-24 16:57:43 +0200

    Create dir templates add head.php

--- index.php
+++ index.php
@@ -1,32 +1,20 @@
 <?php 
     require __DIR__ . '/database/db.php';
+    require __DIR__ . '/templates/head.php'
 
-    /* echo '<pre>';
-    var_dump($discs);
-    echo '</pre>'; */
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<header id="site_header">
+    <div class="logo">
+        <img width="60px" src="./dist/img/spotify-2015-logo.png" alt="">
+    </div>
+</header>
 
-<body>
-    <header>
-        <div class="logo">
-            <img width="60px" src="" alt="">
-        </div>
-    </header>
+<main>
 
-    <main>
-
-    </main>
+</main>
 </body>
 
 </html>
