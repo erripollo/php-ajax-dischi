bd2cc4926dd6349ba94c95c2acb929a7127740da
erripollo <pollone.enrico@gmail.com>
2021-06-24 19:01:39 +0200

    display discs by Vue. Complete Milestone_2

--- Milestone_2/templates/main.php
+++ Milestone_2/templates/main.php
@@ -2,14 +2,12 @@
 <main id="site_main">
 
     <!-- cards album -->
-    <?php foreach ($discs as $disc) : ?>
-    <div class="card">
-        <img width="160px" src="<?= $disc['poster']; ?>" alt="album cover image">
-        <div class="title"><?= $disc['title']; ?></div>
-        <div class="author"><?= $disc['author']; ?></div>
-        <div class="year"><?= $disc['year']; ?></div>
+    <div class="card" v-for="disc in discs">
+        <img width="160px" :src="disc.poster" alt="disc cover image">
+        <div class="title">{{disc.title}}</div>
+        <div class="author">{{disc.author}}</div>
+        <div class="year">{{disc.year}}</div>
     </div>
-    <?php endforeach; ?>
     <!-- /cards album -->
 
 </main>
