32f2b0ae5255adcfcc25bbf1d4f343ddba8b3557
erripollo <pollone.enrico@gmail.com>
2021-06-24 17:22:25 +0200

    display discs data through foreach

--- index.php
+++ index.php
@@ -5,16 +5,31 @@
 
 ?>
 
-
+<!-- Site header -->
 <header id="site_header">
     <div class="logo">
         <img width="60px" src="./dist/img/spotify-2015-logo.png" alt="">
     </div>
 </header>
+<!-- /Site header -->
 
-<main>
+<!-- Site main -->
+<main id="site_main">
+
+    <!-- card album -->
+    <?php foreach ($discs as $disc) : ?>
+    <div class="card">
+        <img width="160px" src="<?= $disc['poster']; ?>" alt="album cover image">
+        <div class="title"><?= $disc['title']; ?></div>
+        <div class="author"><?= $disc['author']; ?></div>
+        <div class="year"><?= $disc['year']; ?></div>
+    </div>
+    <?php endforeach; ?>
+    <!-- /card album -->
 
 </main>
+<!-- /Site main -->
+
 </body>
 
 </html>
